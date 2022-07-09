8219293fb2c43138878411aeecf0b86d7a46116e
junsj119 <wjstjddud119@naver.com>
2022-07-09 22:36:21 +0900

    07/09

---
+++ data-jpa/src/main/java/com/study/datajpa/entity/Team.java
@@ -0,0 +1,25 @@
+package com.study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username"})   //연관관계 관계는 toString 피하자!
+public class Team {
+    @Id @GeneratedValue
+    @Column(name="team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name){
+        this.name = name;
+    }
+}
